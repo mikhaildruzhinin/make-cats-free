41f01d8567ae85e953bde31a99ab7a2d7079a5a2
Дружинин Михаил Юрьевич <druzhininmy@gmail.com>
2025-03-08 13:00:33 +0300

    update Lifecycle.onStart

--- src/main/java/ru/mikhaildruzhinin/mcf/taskmanagement/Lifecycle.java
+++ src/main/java/ru/mikhaildruzhinin/mcf/taskmanagement/Lifecycle.java
@@ -1,19 +1,29 @@
 package ru.mikhaildruzhinin.mcf.taskmanagement;
 
-import io.quarkus.elytron.security.common.BcryptUtil;
+import io.quarkus.hibernate.reactive.panache.Panache;
 import io.quarkus.runtime.StartupEvent;
+import io.quarkus.vertx.VertxContextSupport;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Observes;
+import jakarta.inject.Inject;
 import org.jboss.logging.Logger;
+import ru.mikhaildruzhinin.mcf.taskmanagement.user.UserRepository;
 
 @ApplicationScoped
 public class Lifecycle {
 
     private static final Logger logger = Logger.getLogger(Lifecycle.class);
 
+    @Inject
+    UserRepository userRepository;
+
     // TODO create CLI command
-    void onStart(@Observes StartupEvent event) {
-        String password = BcryptUtil.bcryptHash("admin");
-        logger.info(password);
+    void onStart(@Observes StartupEvent event) throws Throwable {
+        String errorMessage = "User with an admin role is not found. Use CLI to create one.";
+        VertxContextSupport.subscribeAndAwait(() ->
+                Panache.withSession(() -> userRepository.findByRole("admin")
+                        .onItem().ifNull().failWith(new RuntimeException(errorMessage)))
+                        .onFailure().invoke(f -> logger.error(errorMessage))
+        );
     }
 }
--- src/main/java/ru/mikhaildruzhinin/mcf/taskmanagement/user/UserRepository.java
+++ src/main/java/ru/mikhaildruzhinin/mcf/taskmanagement/user/UserRepository.java
@@ -1,8 +1,13 @@
 package ru.mikhaildruzhinin.mcf.taskmanagement.user;
 
 import io.quarkus.hibernate.reactive.panache.PanacheRepository;
+import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class UserRepository implements PanacheRepository<User> {
+
+    public Uni<User> findByRole(String role) {
+        return find("select 1 from User where role = ?1", role).firstResult();
+    }
 }
